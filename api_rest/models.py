80c53849e69045441eedbf7b56ac09f48f7c4510
Théo <cobaltium360@gmail.com>
2024-01-26 18:52:54 +0100

    ajout du jwt token et refresh token natif, et try de models

--- api_rest/models.py
+++ api_rest/models.py
@@ -8,3 +8,35 @@ class MyUser(AbstractUser):
     can_be_contacted = models.BooleanField(default=True)
     can_data_be_shared = models.BooleanField(default=True)
     REQUIRED_FIELDS = ['age', 'can_be_contacted', 'can_data_be_shared']
+
+
+# class Project(models.Model):
+#     name = models.CharField(max_length=255)
+#     description = models.TextField()
+#     project_type = models.CharField(max_length=20, choices=[('backend', 'Back-end'), ('frontend', 'Front-end'), ('ios', 'iOS'), ('android', 'Android')])
+#     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project_author')
+#     contributors = models.ManyToManyField(User, related_name='project_contributors')
+#     created_time = models.DateTimeField(auto_now_add=True)
+
+
+# class Issue(models.Model):
+#     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=255)
+#     description = models.TextField()
+#     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_issues')
+#     priority = models.CharField(max_length=10, choices=[('LOW', 'Low'), ('MEDIUM', 'Medium'), ('HIGH', 'High')])
+#     tag = models.CharField(max_length=10, choices=[('BUG', 'Bug'), ('FEATURE', 'Feature'), ('TASK', 'Task')])
+#     status = models.CharField(max_length=20, choices=[('To Do', 'To Do'), ('In Progress', 'In Progress'), ('Finished', 'Finished')], default='To Do')
+#     created_time = models.DateTimeField(auto_now_add=True)
+
+# class Comment(models.Model):
+#     issue = models.ForeignKey(Issue, on_delete=models.CASCADE, related_name='comments')
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     content = models.TextField()
+#     unique_identifier = models.UUIDField(default=uuid.uuid4, editable=False)
+#     created_time = models.DateTimeField(auto_now_add=True)
+
+# class Contributor(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+#     created_time = models.DateTimeField(auto_now_add=True)
--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('', views.getData),
-    path("test", views.UserLoginView.as_view())
+    path("test", views.UserLoginView.as_view()),
+    path("token", TokenObtainPairView.as_view()),
+    path("refresh", TokenRefreshView.as_view()),
 ]
